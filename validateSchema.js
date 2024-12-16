611d1464131e44faced0bdaba2d8cd417b79e184
Rahul Patel <140371157+appy-spec@users.noreply.github.com>
2024-12-17 03:54:29 +0530

    Update validateSchema.js

--- validateSchema.js
+++ validateSchema.js
@@ -18,4 +18,14 @@ const listingSchema= Joi.object({
 
 });
 
-module.exports= listingSchema;
+const reviewSchema= Joi.object({
+
+    review:Joi.object({
+
+        rating:Joi.number().min(1).max(5).required(),
+        comment:Joi.string().required(),
+
+    }).required()
+});
+
+module.exports= {listingSchema,reviewSchema};
