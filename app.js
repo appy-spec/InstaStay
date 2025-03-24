aaef3b0c933066994871ebe41438854396231d3d
Rahul Patel <140371157+appy-spec@users.noreply.github.com>
2025-03-25 01:23:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+if(process.env.NODE_ENV!="production"){
+    require('dotenv').config();
+}
+
 const express= require("express");
 const ejs=require("ejs");
 const path=require("path");
 const mongoose=require("mongoose");
-const Listing=require("./modals/model.js");
-const Review=require("./modals/review.js");
 const methodoverride=require("method-override");
 const ejsmate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const wrapAsync= require("./utils/wrapAsync.js");
 const Joi= require("joi");
-const {listingSchema, reviewSchema} =require("./validateSchema.js");
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
+
+const session=require("express-session");
+const MongoStore = require('connect-mongo');
+const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const passportLocalMongoose=require("passport-local-mongoose");
+const User=require("./modals/user.js");
 
 const app=express();
 const port=3000;
@@ -22,162 +34,98 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodoverride("_method"));
 
+// url for both mongo and mong-Atlas database
+
+const mongo_url="mongodb://127.0.0.1:27017/InstaStay";
+const dbUrl=process.env.ATLASDB_URL;
+
+// this is used in mongo-connect or mongo session
+
+const store= MongoStore.create({
+
+    mongoUrl:dbUrl,
+    crypto:{
+
+        secret:process.env.SECRET,
+    },
+    touchAfter:24*3600,
+
+});
+
+store.on("error", ()=>{
+
+    console.log("Error in mongo session store", err);
+});
+
+// this is used in express-session
+
+const sessionOption={
+    
+    store,
+    secret:process.env.SECRET,
+    resave:false,
+    saveUninitialized: true,
+    cookie:{
+        
+      expires:Date.now()+7*24*60*60*1000,
+      maxAge:7*24*60*60*1000,
+      httpOnly:true,
+      
+    }
+};
+
+app.use(session(sessionOption));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// middleware for flash
+
+app.use((req,res,next)=>{
+
+    res.locals.success=req.flash("Success");
+    res.locals.failure=req.flash("Failure");
+    res.locals.currUser=req.user;
+    next();
+});
+
 app.listen(port, ()=>{
 
     console.log("server is running on port 3000");
 });
 
+// this is for mongoose startup
+
 main().then(()=>{
 
-    console.log("successfully start mongoose");
+    console.log("successfully connect to mongoAtlas");
 }).catch((err)=>{
 
     console.log(err);
 });
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/InstaStay");
+
+  await mongoose.connect(dbUrl);
 
 };
 
-const validateListing=((req, res, next)=>{
+// this routes goes to routes files
 
-    let{error}=listingSchema.validate(req.body);
+app.use("/listings", listingRouter);
 
-    if(error){
+app.use("/listings/:id/reviews", reviewRouter);
 
-        throw new ExpressError(400, error);
-    }
-    else{
-
-        next();
-    }
-
-});
-
-const validateReview=((req, res, next)=>{
-
-    let{error}=reviewSchema.validate(req.body);
-
-    if(error){
-       
-        throw new ExpressError(400, error);
-    }
-    else{
-
-        next();
-    }
-
-});
+app.use("/", userRouter); 
 
 app.get("/", (req,res)=>{
 
     res.send("this is trail page");
-});
-
-// view all listing route
-
-app.get("/listings", wrapAsync(async(req,res, next)=>{
-    
-    let allListing=await Listing.find({});
-    res.render("listings/index.ejs", {allListing});
-}));
-
-// new listing add route
-
-app.get("/listings/new", (req,res)=>{
-
-    res.render("listings/new.ejs");
-});
-
-app.post("/listings", validateListing, (req,res, next)=>{
-
-    let{newlisting}=req.body;
-    let addlisting=new Listing(newlisting);
-    addlisting.save().then(()=>{
-
-        console.log("saved successfully!!!!");
-    }).catch((err)=>{
-
-        next(err);
-    });
-
-    res.redirect("/listings");
-
-});
-
-// particular listing view route
-
-app.get("/listings/:id", wrapAsync(async(req,res, next)=>{
-
-    let{id}=req.params;
-
-    let listing=await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", {listing});
-
-}));
-
-// listing update route
-
-app.get("/listings/:id/edit", wrapAsync(async(req,res)=>{
-
-    let{id}=req.params;
-    
-    let listing=await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-
-}));
-
-app.put("/listings/:id", validateListing, wrapAsync(async(req,res)=>{
-
-    let{id}=req.params;
-    let{newlisting}=req.body;
-    
-    await Listing.findByIdAndUpdate(id,{...newlisting},{ runValidators: true });
-    res.redirect("/listings");
-    
-}));
-
-// listing delete route
-
-app.delete("/listings/:id", wrapAsync(async(req,res)=>{
-
-    let {id}=req.params;
-
-    let deleteListing=await Listing.findByIdAndDelete(id);
-    console.log(deleteListing);
-
-    res.redirect("/listings");
-
-}));
-
-// new reviews add route
-
-app.post("/listings/:id/reviews",validateReview, wrapAsync(async(req, res, next)=>{
-
-    let{id}=req.params;
-    let{review}=req.body;
-    let listing= await Listing.findById(id);
-    let addReview=new Review(review);
-
-    listing.reviews.push(addReview);
-    await addReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${id}`);
-}));
-
-// delete reviews route
-
-app.delete("/listings/:id/reviews/:reviewId", async(req, res, next)=>{
-
-    let{id,reviewId}=req.params;
-
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-
 });
 
 // error handling route
@@ -194,8 +142,6 @@ app.use((err, req, res, next)=>{
 
     res.render("listings/error.ejs", {message});
 });
-
-
 
 
 
